c320b7284b42603d03e2c73771fa96ba3372347e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-07-14 16:58:22 -0500

    Initial commit.

---
+++ CaelumGameManager/CaelumGameManagerGUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CaelumGameManagerGUI.Models;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaelumGameManagerGUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+
+    }
+}
