14dbd4f524064a9570966c0879ff9707d1805c1e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-07-15 10:18:07 -0500

    Early working package list display.

--- CaelumGameManager/CaelumGameManagerGUI/ViewModels/ShellViewModel.cs
+++ CaelumGameManager/CaelumGameManagerGUI/ViewModels/ShellViewModel.cs
@@ -1,6 +1,7 @@
 ﻿// using CaelumGameManagerGUI.Models;
+using CaelumCoreLibrary.CaelumPackages;
+using CaelumGameManagerGUI.Models;
 using Caliburn.Micro;
-using PhosLibrary.Common.MusicData;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,39 +13,17 @@ namespace CaelumGameManagerGUI.ViewModels
 {
     public class ShellViewModel : Screen
     {
-        private MusicData _currentMusic = new() { songs = new Song[] { new Song { name = "test" } }};
-        private string _status = "Waiting...";
+        private BindableCollection<PackageModel> packages = new();
 
-        public string Status
+        public ShellViewModel()
         {
-            get { return _status; }
-            set 
-            { 
-                _status = value;
-                NotifyOfPropertyChange(() => Status);
-            }
+
         }
 
-
-        public MusicData CurrentMusic
+        public BindableCollection<PackageModel> Packages
         {
-            get => _currentMusic;
-            set 
-            { 
-                _currentMusic = value;
-                NotifyOfPropertyChange(() => CurrentMusic);
-                NotifyOfPropertyChange(() => Songs);
-            }
+            get { return packages; }
+            set { packages = value; }
         }
-
-        public void LoadMusic()
-        {
-            CurrentMusic = MusicDataParser.Parse($@"{Directory.GetCurrentDirectory()}\current-music-data.json");
-            Status = "Loaded Music";
-            var builder = new PhosLibrary.Games.MusicP4G();
-            builder.Build(CurrentMusic, "./music-build", false);
-        }
-
-        public Song[] Songs => CurrentMusic.songs; 
     }
 }
