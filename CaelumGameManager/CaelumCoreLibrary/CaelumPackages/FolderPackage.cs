0a38496654173a55f183485ac9e499d7ccd402af
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-07-15 09:15:46 -0500

    Folders named Package get ignored with the default VS gitignore lol. Added early implementations of package types.

---
+++ CaelumGameManager/CaelumCoreLibrary/CaelumPackages/FolderPackage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaelumCoreLibrary.Packages
+{
+    public class FolderPackage : IPackage
+    {
+        /// <inheritdoc/>
+        public string Name { get; set; }
+
+        /// <inheritdoc/>
+        public string[] Authors { get; set; }
+
+        /// <inheritdoc/>
+        public string Game { get; set; }
+
+        /// <inheritdoc/>
+        public string Version { get; set; }
+    }
+}
