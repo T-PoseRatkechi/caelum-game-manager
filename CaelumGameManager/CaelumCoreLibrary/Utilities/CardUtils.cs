b42a6862cb5a335fbb8976eb8feaa365466a863e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-07-17 18:48:19 -0500

    Added card utility function that checks if a string is a valid card ID.

---
+++ CaelumGameManager/CaelumCoreLibrary/Utilities/CardUtils.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) T-Pose Ratkechi. All rights reserved.
+// Licensed under the GNU GPLv3 license. See LICENSE file in the project root for full license information.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+namespace CaelumCoreLibrary.Utilities
+{
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Utility functions related to Cards.
+    /// </summary>
+    public class CardUtils
+    {
+        /// <summary>
+        /// Returns whether <paramref name="s"/> is valid as an ID. Only A-z and - characters.
+        /// </summary>
+        /// <param name="s">String to test.</param>
+        /// <returns>If <paramref name="s"/> is a valid <seealso cref="Cards.ICard"/> ID.</returns>
+        public static bool IsValidId(string s)
+        {
+            Regex reg = new(@"([^A-z^0-9-])");
+
+            if (reg.IsMatch(s))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
