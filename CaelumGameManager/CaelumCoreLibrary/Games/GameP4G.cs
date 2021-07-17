d7529928f2b31a91745d8784514b09b7ca024d6e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-07-17 18:48:39 -0500

    Early implementations of Games.

---
+++ CaelumGameManager/CaelumCoreLibrary/Games/GameP4G.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) T-Pose Ratkechi. All rights reserved.
+// Licensed under the GNU GPLv3 license. See LICENSE file in the project root for full license information.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+namespace CaelumCoreLibrary.Games
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CaelumCoreLibrary.Cards;
+
+    /// <summary>
+    /// IGame implementation for P4G.
+    /// </summary>
+    public class GameP4G : IGame
+    {
+        private string gamePath;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameP4G"/> class.
+        /// </summary>
+        public GameP4G()
+        {
+            this.Name = "Persona 4 Golden";
+            this.Init();
+        }
+
+        /// <inheritdoc/>
+        public string Name { get; init; }
+
+        /// <inheritdoc/>
+        public ICard CreateCard(string name, CardType type)
+        {
+            switch (type)
+            {
+                case CardType.Folder:
+                    var cardFolderPath = Path.Join(this.gamePath, name);
+                    return new FolderCard(cardFolderPath);
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// Handles initializing game folders and files.
+        /// </summary>
+        private void Init()
+        {
+            var gameDir = Directory.CreateDirectory(Path.Join(Directory.GetCurrentDirectory(), this.Name));
+            this.gamePath = gameDir.FullName;
+        }
+    }
+}
---
+++ CaelumGameManager/CaelumCoreLibrary/Games/IGame.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) T-Pose Ratkechi. All rights reserved.
+// Licensed under the GNU GPLv3 license. See LICENSE file in the project root for full license information.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+namespace CaelumCoreLibrary.Games
+{
+    using CaelumCoreLibrary.Cards;
+
+    /// <summary>
+    /// Game interface for managing deck and card creation.
+    /// </summary>
+    public interface IGame
+    {
+        /// <summary>
+        /// Gets game name.
+        /// </summary>
+        public string Name { get; init; }
+
+        /// <summary>
+        /// Handles creating a new card with the given name and <seealso cref="CardType"/>.
+        /// </summary>
+        /// <param name="name">Name of card.</param>
+        /// <param name="type">Card type.</param>
+        /// <returns>New <seealso cref="ICard"/> instance.</returns>
+        public ICard CreateCard(string name, CardType type);
+    }
+}
