91fc6667be1b77be23169de3f1274761dcd24d6a
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-07-16 22:39:14 -0500

    Started implemention of reading and writing decks. Extended DeckView to fill all space.

---
+++ CaelumGameManager/CaelumCoreLibrary/Decks/DeckIO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaelumCoreLibrary.Decks
+{
+    public class DeckIO
+    {
+        public static void ParseDeck(string deckPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void WriteDeck(string deckPath, DeckModel deck)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CaelumGameManager/CaelumCoreLibrary/Decks/DeckModel.cs
@@ -0,0 +1,17 @@
+﻿using CaelumCoreLibrary.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaelumCoreLibrary.Decks
+{
+    public class DeckModel
+    {
+        public string Game { get; set; }
+        public string Format { get; set; }
+        public string Name { get; set; }
+        public ICard[] Cards { get; set; }
+    }
+}
