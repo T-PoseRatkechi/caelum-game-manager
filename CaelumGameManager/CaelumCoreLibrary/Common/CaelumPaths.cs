f97b7332f20f63a33f8cf51fd257b13fcacd3eb1
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-07-18 01:21:29 -0500

    Hopefully temporary method for sharing paths not specific to a single game, like the tools directory.

---
+++ CaelumGameManager/CaelumCoreLibrary/Common/CaelumPaths.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) T-Pose Ratkechi. All rights reserved.
+// Licensed under the GNU GPLv3 license. See LICENSE file in the project root for full license information.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+namespace CaelumCoreLibrary.Common
+{
+    using System.IO;
+
+    /// <summary>
+    /// Contains paths don't relate to single game.
+    /// </summary>
+    public class CaelumPaths
+    {
+        /// <summary>
+        /// Gets path of tools directory.
+        /// </summary>
+        public static string ToolsDir => Path.Join(Directory.GetCurrentDirectory(), "tools");
+    }
+}
