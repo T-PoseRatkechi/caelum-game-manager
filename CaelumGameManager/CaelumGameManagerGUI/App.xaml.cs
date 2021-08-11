5c1c8cfc84bb69187b8a9b08125d50653cfbe13d
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-08-10 22:03:51 -0500

    WIP Logging with Serilog

--- CaelumGameManager/CaelumGameManagerGUI/App.xaml.cs
+++ CaelumGameManager/CaelumGameManagerGUI/App.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿// Copyright (c) T-Pose Ratkechi. All rights reserved.
+// Licensed under the GNU GPLv3 license. See LICENSE file in the project root for full license information.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
 namespace CaelumGameManagerGUI
 {
+    using System.Windows;
+    using Microsoft.Extensions.Configuration;
+    using Serilog;
+
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for App.xaml.
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            Log.Information("Caelum Game Manager starting");
+        }
     }
 }
