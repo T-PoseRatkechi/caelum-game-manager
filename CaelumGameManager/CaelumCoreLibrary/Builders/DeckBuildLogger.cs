77789c1f5211306e0ddf8c82428000587f54bd30
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-08-21 08:16:28 -0500

    Created deck build logger to handle logging output files.

---
+++ CaelumGameManager/CaelumCoreLibrary/Builders/DeckBuildLogger.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) T-Pose Ratkechi. All rights reserved.
+// Licensed under the GNU GPLv3 license. See LICENSE file in the project root for full license information.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+namespace CaelumCoreLibrary.Builders
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using CaelumCoreLibrary.Cards;
+
+    /// <summary>
+    /// Responsible for logging a deck's output build.
+    /// </summary>
+    public class DeckBuildLogger
+    {
+        private readonly Dictionary<string, List<string>> outputFilesLog = new();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeckBuildLogger"/> class.
+        /// </summary>
+        public DeckBuildLogger()
+        {
+        }
+
+        /// <summary>
+        /// Adds to output log that <paramref name="card"/> outputted <paramref name="file"/>.
+        /// </summary>
+        /// <param name="card">Card that outputted file.</param>
+        /// <param name="outputFile">File that was outputted.</param>
+        public void LogOutputFile(CardModel card, string outputFile)
+        {
+            if (this.outputFilesLog.ContainsKey(outputFile))
+            {
+                this.outputFilesLog[outputFile].Add(card.CardId);
+            }
+            else
+            {
+                this.outputFilesLog.Add(outputFile, new());
+            }
+        }
+
+        /// <summary>
+        /// Writes the output build log to file.
+        /// </summary>
+        /// <param name="outputFile">Log output file.</param>
+        public void WriteOutputLog(string outputFile)
+        {
+            // Output build log.
+            StringBuilder sb = new();
+            foreach (var entry in this.outputFilesLog)
+            {
+                sb.AppendLine($"File: {entry.Key}\nCards: {string.Join(", ", entry.Value)}\n");
+            }
+
+            File.WriteAllText(outputFile, sb.ToString());
+        }
+    }
+}
