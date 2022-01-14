e39865536700cdf3d4d0f4167d9a6dab5aec2ce9
T-Pose Ratkechi <tposeratkechi@gmail.com>
2022-01-14 14:11:58 -0600

    .

---
+++ CaelumGameManager/CaelumGameManagerGUI/Views/Controls/SelectButtonView.xaml.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) T-Pose Ratkechi. All rights reserved.
+// Licensed under the GNU GPLv3 license. See LICENSE file in the project root for full license information.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+namespace CaelumGameManagerGUI.Views.Controls
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for SelectButtonView.xaml.
+    /// </summary>
+    public partial class SelectButtonView : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelectButtonView"/> class.
+        /// </summary>
+        public SelectButtonView()
+        {
+            this.InitializeComponent();
+        }
+
+
+    }
+}
